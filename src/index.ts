b6c75fc6e9127b1a68ef8ac868bee9c24104fa01
AkShAy21211 <akshay68580@gmail.com>
2025-02-02 16:18:18 +0530

    feat/ added express app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('ff');
+import ExpressApp from "./infrastructure/app/app.js";
+
+const start = () => {
+  const app = new ExpressApp();
+  const PORT = process.env.PORT||3000;
+  try {
+    app.startServer(PORT);
+  } catch (error) {
+    process.exit(1);
+  }
+};
+
+start();
